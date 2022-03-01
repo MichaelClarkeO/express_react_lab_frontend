4d7d717758ef8e385eca24c376b964b26b60e76f
oclarkem23 <mikeyj913@hotmail.com>
2022-03-01 18:45:35 -0500

    Running portfolio

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import{Routes, Route, Router } from "react-router-dom"
+import{Routes, Route,  } from "react-router-dom"
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -14,13 +14,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/projects" element={ <Projects URL={URL} />} />
           <Route path="/about" element={<About URL={URL} />} />
         </Routes>
-      </Router>
       <Footer />
     </div>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,33 +1,34 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-function Header(props) {
-  //inline style for the nav tag
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    border: "3px solid black",
-    padding: "8px",
-    width: "90%",
-    margin: "auto",
+function About(props) {
+  // create state to hold about data
+  const [about, setAbout] = useState(null);
+
+  // create function to make api call
+  const getAboutData = async () => {
+    // make api call and get response
+    const response = await fetch(props.URL + "about");
+    // turn response into javascript object
+    const data = await response.json();
+    // set the about state to the data
+    setAbout(data);
   };
 
-  return (
-    <header>
-      <h1>My Portfolio Page</h1>
-      <nav style={navStyle}>
-        <Link to="/">
-          <div>HOME</div>
-        </Link>
-        <Link to="/about">
-          <div>ABOUT</div>
-        </Link>
-        <Link to="/projects">
-          <div>PROJECTS</div>
-        </Link>
-      </nav>
-    </header>
+  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  useEffect(() => getAboutData());
+
+  // define a function that will return the JSX needed once we get the data
+  const loaded = () => (
+    <div>
+      <h2>{about.name}</h2>
+      <h3>{about.email}</h3>
+      <p>{about.bio}</p>
+    </div>
   );
+
+  // if data arrives return the result of loaded, if not, an h1 that says loading
+  return about ? loaded() : <h1>Loading...</h1>;
 }
 
-export default Header;
+export default About;
