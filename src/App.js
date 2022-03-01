8cc387d3a83a8314e4f3b5f90bfc735cbd3a8467
oclarkem23 <mikeyj913@hotmail.com>
2022-03-01 18:12:23 -0500

    netlify setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import{Route, Switch } from "react-router-dom"
+import{Routes, Route, Router } from "react-router-dom"
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -14,17 +14,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/projects">
-          <Projects URL={URL} />
-        </Route>
-        <Route path="/about">
-          <About URL={URL} />
-        </Route>
-      </Switch>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={ <Projects URL={URL} />} />
+          <Route path="/about" element={<About URL={URL} />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
